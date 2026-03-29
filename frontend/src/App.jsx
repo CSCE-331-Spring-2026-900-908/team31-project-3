8a59956fadce521634f0e01d289dd590e797feb2
“abelasteway” <astewayabel@gmail.com>
2026-03-29 14:53:11 -0500

    testing changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,23 @@
 import React from "react";
 import "./App.css";
+import Message from "./InventorySearchbar";
+
 
 const App = () => {
-  return <div>App</div>;
+  const allitems = ['New York', 'San Francisco', 'Paris', 'London']
+
+  const handleSelectItem = (item) => {
+    console.log(item);
+  }
+  return (
+    <div>
+      <Message
+        items={allitems}
+        heading="helloHeading" 
+        onSelect={handleSelectItem}
+      />
+    </div>
+    )
 };
 
 export default App;
