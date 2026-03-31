4912f00960743b62d25ed32cbaee30a8c82f3e14
“abelasteway” <astewayabel@gmail.com>
2026-03-31 13:52:36 -0500

    Expermenting with chatBot

--- frontend/src/pages/Chatbot.jsx
+++ frontend/src/pages/Chatbot.jsx
@@ -1,13 +1,66 @@
 import './Chatbot.css';
+import {useEffect, useState, useRef } from 'react';
+
+const BASE_URL = 'https://jsonplaceholder.typicode.com';
+
 
 // chat bot function
 const Chatbot = () => {
+
+    const [posts, setPosts] = useState([]);
+    const [isLoading, setLoading] = useState(false);
+    const [error, setError] = useState();
+
+    const abortControllerRef = useRef(null);
+
+    useEffect(() => {
+        const fetchPost = async () => {
+            abortControllerRef.current?.abort();
+            abortControllerRef.current = new AbortController();
+            setLoading(true);
+            try{
+                const response = await fetch(`${BASE_URL}/posts`, {
+                    signal: abortControllerRef.current?.signal
+                });
+                const data = await response.json();
+                setPosts(data);
+            }
+            catch(e){
+                if(e.name === 'AbortError'){
+                    console.log('Aborted');
+                    return;
+                }
+                setError(e);
+            }
+            finally{
+                setLoading(false);
+            }
+        }
+
+        fetchPost();
+    }, []); 
+
+    if(isLoading){
+        return <div>Loading...</div>
+    }
+
+    if(error){
+        return <div>iError in the database</div>
+    }
+ 
   return (
-    
     <div className="Chatbot-Wrapper">
       <h1>Hello</h1>
+        <div>
+            <input placehodler = "Hello there"></input>
+        </div>
+        <ul>
+            {posts.map((post) => {
+                return <li key ={post.id}>{post.title}</li>
+            })}
+        </ul>
+      
     </div>
-    
   )
 }
 
