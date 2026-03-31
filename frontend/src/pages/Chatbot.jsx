cdbc5af6365fa5c09f0a76950b2e5cbacb1a09b8
“abelasteway” <astewayabel@gmail.com>
2026-03-30 21:41:08 -0500

    Removing bad lines

--- frontend/src/pages/Chatbot.jsx
+++ frontend/src/pages/Chatbot.jsx
@@ -1,8 +1,8 @@
 import './Chatbot.css';
-//Testing changes more changes
+
 const Chatbot = () => {
   return (
-    //finding
+    
     <div className="Chatbot-Wrapper">
       <h1>Hello</h1>
     </div>
