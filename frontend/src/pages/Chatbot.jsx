1e402d981bf7bf97e06afd800e19d426da5d252f
“abelasteway” <astewayabel@gmail.com>
2026-04-01 13:30:49 -0500

    making the chatbot look preitter

--- frontend/src/pages/Chatbot.jsx
+++ frontend/src/pages/Chatbot.jsx
@@ -1,6 +1,7 @@
 import './Chatbot.css';
 import {useEffect, useState, useRef } from 'react';
 import { sendGemniMessage } from './ChatbotGemni';
+import bobamascot from "../assets/bobamascot.png";
 
 const BASE_URL = 'https://jsonplaceholder.typicode.com';
 
@@ -8,48 +9,48 @@ const BASE_URL = 'https://jsonplaceholder.typicode.com';
 // chat bot function
 const Chatbot = () => {
 
-    const [posts, setPosts] = useState([]);
-    const [isLoading, setLoading] = useState(false);
-    const [error, setError] = useState();
+    // const [posts, setPosts] = useState([]);
+    // const [isLoading, setLoading] = useState(false);
+    // const [error, setError] = useState();
     const [userInput, setUserInput] = useState("");
     const [aiOutput, setaiOutput] = useState("");
+    const [isVisible, setVisible] = useState(false);
+    // const abortControllerRef = useRef(null);
 
-    const abortControllerRef = useRef(null);
+    // useEffect(() => {
+    //     const fetchPost = async () => {
+    //         abortControllerRef.current?.abort();
+    //         abortControllerRef.current = new AbortController();
+    //         setLoading(true);
+    //         try{
+    //             const response = await fetch(`${BASE_URL}/posts`, {
+    //                 signal: abortControllerRef.current?.signal
+    //             });
+    //             const data = await response.json();
+    //             setPosts(data);
+    //         }
+    //         catch(e){
+    //             if(e.name === 'AbortError'){
+    //                 console.log('Aborted');
+    //                 return;
+    //             }
+    //             setError(e);
+    //         }
+    //         finally{
+    //             setLoading(false);
+    //         }
+    //     }
 
-    useEffect(() => {
-        const fetchPost = async () => {
-            abortControllerRef.current?.abort();
-            abortControllerRef.current = new AbortController();
-            setLoading(true);
-            try{
-                const response = await fetch(`${BASE_URL}/posts`, {
-                    signal: abortControllerRef.current?.signal
-                });
-                const data = await response.json();
-                setPosts(data);
-            }
-            catch(e){
-                if(e.name === 'AbortError'){
-                    console.log('Aborted');
-                    return;
-                }
-                setError(e);
-            }
-            finally{
-                setLoading(false);
-            }
-        }
+    //     fetchPost();
+    // }, []); 
 
-        fetchPost();
-    }, []); 
+    // if(isLoading){
+    //     return <div>Loading...</div>
+    // }
 
-    if(isLoading){
-        return <div>Loading...</div>
-    }
-
-    if(error){
-        return <div>iError in the database</div>
-    }
+    // if(error){
+    //     return <div>iError in the database</div>
+    // }
     
     async function handleSend(){
         const reply = await sendGemniMessage(userInput);
@@ -64,27 +65,50 @@ const Chatbot = () => {
         }
     }
 
+    function toggleVisible(){
+        if(isVisible === true)
+            setVisible(false);
+        else{
+            setVisible(true);
+        }
+    }
 
   return (
-    <div className="Chatbot-Wrapper">
-      <h1>Hello</h1>
-      <h1> YES {userInput} </h1>
-        <div>
-            <input 
-            placehodler = "Hello there"
-            type = 'text'
-            value = {userInput}
-            onChange ={(e) => setUserInput(e.target.value)}
-            onKeyDown={handleEnter}
-            ></input>
-            <button onClick = {handleSend} >Submit</button>
-        </div>
-        <h1>Response Ai: {aiOutput}</h1>
-        <ul>
+    <div className = "root-of-chatbot">
+      <div>
+            {isVisible &&
+            <div className="Chatbot-Wrapper">
+                <button onClick ={toggleVisible} >X BUTTON</button>
+                <h1>Hello</h1>
+                <h1> YES {userInput} </h1>
+                <h3>Response Ai: {aiOutput}</h3>
+                <input 
+                placehodler = "Hello there"
+                type = 'text'
+                value = {userInput}
+                onChange ={(e) => setUserInput(e.target.value)}
+                onKeyDown={handleEnter}
+                ></input>
+                <button onClick = {handleSend} >Submit</button>
+                <button onClick ={toggleVisible} >X BUTTON</button>
+            </div>
+            }
+            {!isVisible &&
+            <div className = "masscot-icon">
+                <button onClick = {toggleVisible}>
+                    <img src ={bobamascot}></img>
+                </button>
+            </div>
+            }
+      </div>
+        
+        
+
+        {/* <ul>
             {posts.map((post) => {
                 return <li key ={post.id}>{post.title}</li>
             })}
-        </ul>
+        </ul> */}
       
     </div>
   )
