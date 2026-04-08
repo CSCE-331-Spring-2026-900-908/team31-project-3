9594af043eed80481bf5f6170eef9c6c7bf7058d
“abelasteway” <astewayabel@gmail.com>
2026-04-08 10:56:57 -0500

    made the weather based on location

--- frontend/src/pages/Weather.jsx
+++ frontend/src/pages/Weather.jsx
@@ -1,23 +1,42 @@
 import { getWeather } from "./WeatherAPI";
 import { useEffect, useState } from "react";
-
+import { getLocation } from "./Getlocation";
 
 const Weather = () =>{
     const [weather, setWeather] = useState(null);
+    const [location, setLocation] = useState(null);
 
     useEffect(() => {
         async function loadWeather() {
             const data = await getWeather();
             setWeather(data);
         }
-
         loadWeather();
+    }, []);
+
+    useEffect(() =>{
+        async function loadLoaction(){
+
+        try{
+        const data = await getLocation();
+        setLocation(data)
+        }
+        catch(error){
+            console.log(error);
+        }
+        
+    }
+    loadLoaction();
     }, []);
 
     return(
     <div>
-        <h1>The Weather is {weather ? weather.temp() : "Weather can not be provided"}</h1>
-        
+        <h1>The Weather is {weather ? weather.temp + " in " + weather.city  : "Weather can not be provided"}</h1>
+        <h1>Your location is {
+                    location
+                        ? `${location.latitude}, ${location.longitude}`
+                        : "Loading location..."
+        }</h1>
     </div>
     )
 }
--- frontend/src/pages/WeatherAPI.jsx
+++ frontend/src/pages/WeatherAPI.jsx
@@ -1,16 +1,19 @@
+import { getLocation } from "./Getlocation";
+
 const API_KEY = "e3f87a17d8ced33be2deee93b4c29bca"
-const city = "London"
+
+// https://api.openweathermap.org/data/2.5/weather?q=${city},uk&appid=${API_KEY}&units=imperial
+// api.openweathermap.org/data/2.5/weather?q=${city},uk&APPID=${API_KEY}
 export async function getWeather(){
-    const WEATHER_API_URL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=imperial`
+    const location = await getLocation();
+    const WEATHER_API_URL = `https://api.openweathermap.org/data/2.5/weather?lat=${location.latitude}&lon=${location.longitude}&appid=${API_KEY}&units=imperial`
     const response = await fetch(WEATHER_API_URL);
     
-
+    
     if (!response.ok) {
         throw new Error("Weather request failed");
     }
     const data = await response.json();
-
-    
 
     return {
         temp: data.main.temp,
